70d13a15d3e07e082abb40d320b82347d88842e4
Jessie Ramirez <jessieramirez@Jessies-Air.hsd1.ca.comcast.net>
2018-04-02 00:42:55 -0700

    Made gifd still, added styling . Need to create click events to start gif and add bonus

--- app.js
+++ app.js
@@ -1,68 +1,63 @@
-var foodArray = ["Pizza", "Hot Dogs", "Cupcakes", "Watermelon"]
-var title = $("#title")
+var topics = ["Pizza", "Hot Dogs", "Cupcakes", "Watermelon", "Fruits", "Vegetables"]
 
-
-function foodTitle(){
-    var foodType = $(this).attr("data-name");
-    console.log(foodType)
-    title.prepend("<h5>" + foodType + "</h5>");
-}
-
-function createButtons(){
-    $("#buttonHolder").empty();
-
-    for (var i = 0; i < foodArray.length; i++){
-        var newButton = $("<button>");
-        newButton.addClass("food");
-        newButton.attr("data-name", foodArray[i]);
-        newButton.text(foodArray[i]);
-        $("#buttonHolder").append(newButton);
-    }
-}
-
-$("#addFood").on("click", function(event){
-    event.preventDefault();
-    var food = $("#foodInput").val().trim();
-    foodArray.push(food);
-    title.prepend("<h5>" + food + "</h5>");
-    createButtons();
-});
-
-createButtons();
-
-function clear(){
-    $("#clearBtn").on("click", function(){
-        $("#buttonHolder").empty();
-    })
-}
+function gifDisplay(){
 
 $("button").on("click", function() {
     var food = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      food + "&api_key=dc6zaTOxFJmzC&limit=10";
+    food + "&api_key=dc6zaTOxFJmzC&limit=5";
 
     $.ajax({
-      url: queryURL,
+    url: queryURL,
     method: "GET" 
-    })
-      .then(function(response) {
+    }).then(function(response) {
+
         var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-            var gifDiv = $("<div class='item'>");
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    var gifDiv = $("<div class='float'>");
 
-            var rating = results[i].rating;
-            var p = $("<p>").text("Rating: " + rating);
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
 
-            var foodImage = $("<img>");
-            foodImage.attr("src", results[i].images.fixed_height.url);
+                    var foodImage = $("<img>");
+                    foodImage.attr("src", results[i].images.fixed_height_still.url);
 
             gifDiv.append(p);
             gifDiv.append(foodImage);
-
             $("#gifHolder").prepend(gifDiv);
+
+
+            }
           }
-        }
+        });
     });
+}
+function createButtons(){
+    $("#buttonHolder").empty();
+
+    for (var i = 0; i < topics.length; i++){
+        var newButton = $("<button>");
+        newButton.addClass("foodButton");
+        newButton.attr("data-name", topics[i]);
+        newButton.text(topics[i]);
+        $("#buttonHolder").append(newButton);
+    }
+}
+$(document).on("click", ".foodButton", gifDisplay)
+
+$("#addFood").on("click", function(event){
+    event.preventDefault();
+    var food = $("#foodInput").val().trim();
+    topics.push(food);
+    createButtons();
 });
+
+
+createButtons();
+// gifDisplay()
+
+
+// foodImage.attr("src", results[i].images.fixed_height.url);
+
 
